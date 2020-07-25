2354f71b07e6086fb66b2bd376f3ffdbf9d0e659
Kevinbunny2 <68771491+Kevinbunny2@users.noreply.github.com>
2020-07-25 17:08:02 +0530

    Lk.py

--- userbot/plugins/cool.py
+++ userbot/plugins/cool.py
@@ -6,11 +6,11 @@ import asyncio
 from collections import deque
 
 
-@borg.on(events.NewMessage(pattern=r"\.cool", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.lk", outgoing=True))
 async def _(event):
 	if event.fwd_from:
 		return
-	deq = deque(list("☺️😊☺️😊☺️😊☺️😊"))
+	deq = deque(list("SL🇱🇰SL🇱🇰SL🇱🇰SL🇱🇰SL"))
 	for _ in range(48):
 		await asyncio.sleep(0.1)
 		await event.edit("".join(deq))
